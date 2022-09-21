40c8da57f6b626c6f624c6096e28b3b77a2f5023
Justin <77856809+tinpott@users.noreply.github.com>
2022-09-21 17:55:52 -0400

    Update app.js

--- OverseerDashboard/src/app.js
+++ OverseerDashboard/src/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import MainApp from './mainApp.js'
+import MainApp from './main_app.js'
 /**
  * This level is for global stylings and some general settings, such as queryClientProvider
  * 
