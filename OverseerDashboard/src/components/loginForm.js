f743ee5f4ca29689076ed0539990b586246a0c5e
dyuxiang@vt.edu <dyx31971@gmail.com>
2022-10-13 17:07:05 -0400

    added login form component

---
+++ OverseerDashboard/src/components/loginForm.js
@@ -0,0 +1,15 @@
+import React from "react"
+/**
+ * Our Table component, a collection of header and body
+ */
+const LoginForm = () => {
+	return (
+		// <table>
+		// 	<Header/>
+		// 	<Body/>
+		// </table>
+        <></>
+	)
+}
+
+export default LoginForm
