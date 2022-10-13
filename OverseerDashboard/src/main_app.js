fad9ef0e9fbcfd84629cb52f652341226aad3261
dyuxiang@vt.edu <dyx31971@gmail.com>
2022-10-13 17:54:22 -0400

    Added redirections in navbar

--- OverseerDashboard/src/main_app.js
+++ OverseerDashboard/src/main_app.js
@@ -1,37 +1,26 @@
 import React from "react"
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+
 import CargoLayout from "./layouts/cargo_layout.js"
 import {
 	Route,
-	Router,
 	Routes,
-	Link,
   } from "react-router-dom";
 import LoginForm from "./components/loginForm.js";
 
+import NavBar from "./components/navBar.js";
 /**
  * We do our routings here at this level, as well as global components, such as top nav bar
  * Routings will look like a state machine(doesn't really have to)
  */
 const MainApp = () => {
 	return (
-		<>
-			<AppBar 
-				position="static"
-				style={{ background: '#630031' }}>
-				<Toolbar>
-					<Button color="inherit">Home</Button>
-					<Button color="inherit"> Return to Login</Button>
-				</Toolbar>
-			</AppBar>
-			<Routes>
-				<Route path="/" element={<LoginForm />} />
-				<Route path="/cargos" element={<CargoLayout />} />
-				<Route path="/login" element={<LoginForm />} />
-			</Routes>
+		<>	
+		<NavBar />
+		<Routes>
+			<Route path="/" element={<LoginForm />} />
+			<Route path="/cargos" element={<CargoLayout />} />
+			<Route path="/login" element={<LoginForm />} />
+		</Routes>
 		</>
 
 	)
