692c32a6eacbbb358138d47a9d1e274e5bf5912d
dyuxiang@vt.edu <dyx31971@gmail.com>
2022-10-13 17:06:49 -0400

    Added material ui and navbar

--- OverseerDashboard/src/main_app.js
+++ OverseerDashboard/src/main_app.js
@@ -1,5 +1,8 @@
 import React from "react"
-
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import CargoLayout from "./layouts/cargo_layout.js"
 
 /**
@@ -8,7 +11,18 @@ import CargoLayout from "./layouts/cargo_layout.js"
  */
 const MainApp = () => {
 	return (
-		<CargoLayout/>
+		
+		<>
+		<AppBar 
+			position="static"
+			style={{ background: '#630031' }}>
+			<Toolbar>
+				<Button color="inherit">Home</Button>
+				<Button color="inherit"> Return to Login</Button>
+			</Toolbar>
+		</AppBar>
+		<CargoLayout />
+		</>
 	)
 }
 
