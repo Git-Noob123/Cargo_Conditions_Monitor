7978ab8a496dcad148e319f7eddc3ef936da1d0e
dyuxiang@vt.edu <dyx31971@gmail.com>
2022-10-13 17:43:48 -0400

    Added react router to perform routing

--- OverseerDashboard/src/main_app.js
+++ OverseerDashboard/src/main_app.js
@@ -4,6 +4,13 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import CargoLayout from "./layouts/cargo_layout.js"
+import {
+	Route,
+	Router,
+	Routes,
+	Link,
+  } from "react-router-dom";
+import LoginForm from "./components/loginForm.js";
 
 /**
  * We do our routings here at this level, as well as global components, such as top nav bar
@@ -11,18 +18,22 @@ import CargoLayout from "./layouts/cargo_layout.js"
  */
 const MainApp = () => {
 	return (
-		
 		<>
-		<AppBar 
-			position="static"
-			style={{ background: '#630031' }}>
-			<Toolbar>
-				<Button color="inherit">Home</Button>
-				<Button color="inherit"> Return to Login</Button>
-			</Toolbar>
-		</AppBar>
-		<CargoLayout />
+			<AppBar 
+				position="static"
+				style={{ background: '#630031' }}>
+				<Toolbar>
+					<Button color="inherit">Home</Button>
+					<Button color="inherit"> Return to Login</Button>
+				</Toolbar>
+			</AppBar>
+			<Routes>
+				<Route path="/" element={<LoginForm />} />
+				<Route path="/cargos" element={<CargoLayout />} />
+				<Route path="/login" element={<LoginForm />} />
+			</Routes>
 		</>
+
 	)
 }
 
