d13029533d387c6d501c635ba54e6e5937710e13
Justin Potter <justintpotter@vt.edu>
2022-12-03 16:37:44 -0500

    Add header comments to controllers

--- OverseerDashboard/src/controllers/update_appliances.js
+++ OverseerDashboard/src/controllers/update_appliances.js
@@ -1,3 +1,9 @@
+/* update_appliances.js    Yuxiang Dong, Justin Potter    Virginia Tech    November 2, 2022
+ * This defines a component that sends a PATCH request to the database when the overseer toggles
+ * on/off an appliance
+ * Modified November 3, 2022 to replace hard-coded database URL
+ */
+
 import axios from "axios"
 
 import DatabaseURL from "../components/db_url"
